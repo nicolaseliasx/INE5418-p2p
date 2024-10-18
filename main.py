be17073f88b6aa69f3c0690b5ace8e80162713cc
Nicolas Elias <nicolaselias@pm.me>
2024-10-18 11:01:45 -0300

    feat: frufru

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ from config_parser import Config_Parser
 from peer_node import Peer_Node
 
 def main():
+    print("""
+                                222222222222222                        
+                                2:::::::::::::::22                      
+                                2::::::222222:::::2                     
+                                2222222     2:::::2                     
+            ppppp   ppppppppp               2:::::2 ppppp   ppppppppp   
+            p::::ppp:::::::::p              2:::::2 p::::ppp:::::::::p  
+            p:::::::::::::::::p          2222::::2  p:::::::::::::::::p 
+            pp::::::ppppp::::::p    22222::::::22   pp::::::ppppp::::::p
+            p:::::p     p:::::p  22::::::::222      p:::::p     p:::::p
+            p:::::p     p:::::p 2:::::22222         p:::::p     p:::::p
+            p:::::p     p:::::p2:::::2              p:::::p     p:::::p
+            p:::::p    p::::::p2:::::2              p:::::p    p::::::p
+            p:::::ppppp:::::::p2:::::2       222222 p:::::ppppp:::::::p
+            p::::::::::::::::p 2::::::2222222:::::2 p::::::::::::::::p 
+            p::::::::::::::pp  2::::::::::::::::::2 p::::::::::::::pp  
+            p::::::pppppppp    22222222222222222222 p::::::pppppppp    
+            p:::::p                                 p:::::p            
+            p:::::p                                 p:::::p            
+            p:::::::p                               p:::::::p           
+            p:::::::p                               p:::::::p           
+            p:::::::p                               p:::::::p           
+            ppppppppp                               ppppppppp           
+        """)
+    
     topology = Config_Parser.read_topology('configs/topologia.txt')
     config = Config_Parser.read_config('configs/config.txt')
     
